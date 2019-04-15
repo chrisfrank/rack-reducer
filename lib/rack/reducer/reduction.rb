e5f27e46988eb928d2f753a5baf481dda18c541c
Daniel Puglisi <pulleasy@gmail.com>
2019-04-15 21:55:20 +0200

    apply default filters even if params are empty or nil

--- lib/rack/reducer/reduction.rb
+++ lib/rack/reducer/reduction.rb
@@ -12,6 +12,7 @@ module Rack
       def initialize(dataset, *filters)
         @dataset = dataset
         @filters = filters
+        @default_filters = filters.select{|filter| filter.required_argument_names.empty?}
       end
 
       # Run +@filters+ against the params argument
@@ -19,10 +20,16 @@ module Rack
       #   a Rack-compatible params hash
       # @return +@dataset+ with the matching filters applied
       def apply(params)
-        return @dataset if !params || params.empty?
+        if !params || params.empty?
+          return @dataset if @default_filters.empty?
+          filters = @default_filters
+          symbolized_params = Hash.new
+        else
+          filters = @filters
+          symbolized_params = params.to_unsafe_h.symbolize_keys
+        end
 
-        symbolized_params = params.to_unsafe_h.symbolize_keys
-        @filters.reduce(@dataset) do |data, filter|
+        filters.reduce(@dataset) do |data, filter|
           next data unless filter.satisfies?(symbolized_params)
 
           data.instance_exec(
