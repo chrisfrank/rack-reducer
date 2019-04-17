209aa46fa97b041d18947317c3752feedbc27532
Chris Frank <chris.frank@future.com>
2019-04-17 13:43:36 -0400

    Run Rails specs in forked processes
    Loading Rails adds monkeypatches to the core classes, which were leaking
    into other specs. Running the Rails specs in a separate process fixes
    that problem.

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -1,36 +1,53 @@
 require 'spec_helper'
 require_relative 'fixtures'
-require 'action_controller/railtie'
-require 'securerandom'
 
-class RailsApp < Rails::Application
-  routes.append do
-    get "/", to: "artists#index"
-    get "/query", to: "artists#query"
+Process.respond_to?(:fork) && RSpec.describe('in a Rails app') do
+  BuildRailsApp = lambda do
+    require 'action_controller/railtie'
+    require 'securerandom'
+    app = Class.new(Rails::Application) do
+      routes.append do
+        get "/", to: "artists#index"
+        get "/query", to: "artists#query"
+      end
+
+      config.api_only = true
+      config.eager_load = true
+      config.secret_key_base = SecureRandom.hex(64)
+    end
+
+    ArtistsController = Class.new(ActionController::API) do
+      def index
+        @artists = Fixtures::ArtistReducer.apply(params)
+        render json: @artists
+      end
+
+      def query
+        @artists = Fixtures::ArtistReducer.apply(request.query_parameters)
+        render json: @artists
+      end
+    end
+
+    app.initialize!
   end
 
-  config.api_only = true
-  config.eager_load = true
-  config.secret_key_base = SecureRandom.hex(64)
-end
+  let(:app) { BuildRailsApp.call }
 
-class ArtistsController < ActionController::API
-  def index
-    @artists = Fixtures::ArtistReducer.apply(params)
-    render json: @artists
+  it 'works with ActionController::Parameters' do
+    pid = Process.fork do
+      get('/') { |res| expect(res.status).to eq(200) }
+    end
+    Process.wait pid
   end
 
-  def query
-    @artists = Fixtures::ArtistReducer.apply(request.query_parameters)
-    render json: @artists
+  it 'works with request.query_parameters' do
+    pid = Process.fork do
+      get('/query') { |res| expect(res.status).to eq(200) }
+    end
+    Process.wait(pid)
   end
-end
 
-RSpec.describe RailsApp do
-  let(:app) { RailsApp.initialize! }
-
-  it 'works with ActionController::Parameters and a plain hash' do
-    get('/') { |res| expect(res.status).to eq(200) }
-    get('/query') { |res| expect(res.status).to eq(200) }
+  it 'does not load ActiveSupport into global scope b/c of this spec' do
+    expect { ''.blank? }.to raise_error(NoMethodError)
   end
 end
